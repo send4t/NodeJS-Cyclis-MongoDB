fe63d9b2b0f219b2a12e88b19565f2897176bebc
init <16517372+init4t@users.noreply.github.com>
2023-07-26 14:24:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,12 +24,32 @@ const connectDB = async () => {
 
 
 //Routes go here
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
+
+ 
+var whitelist = ['http://send4t.github.io', 'http://send4t.github.io/poems.html']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+ 
+app.get('/books', cors(corsOptionsDelegate), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for a whitelisted domain.'})
+})
+ 
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
+
+
+
+
+
 
 app.get('/', (req,res) => {
     res.send({ title: 'Books' });
