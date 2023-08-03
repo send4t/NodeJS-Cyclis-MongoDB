d6a534e073da5008dc3fa2f5817b5565be1b927d
init <16517372+init4t@users.noreply.github.com>
2023-08-03 11:43:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,29 +23,7 @@ const connectDB = async () => {
 }
 
 
-//Routes go here
-
  
-var whitelist = ['http://send4t.github.io', 'http://send4t.github.io/poems.html']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
- 
-app.get('/books', cors(corsOptionsDelegate), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for a whitelisted domain.'})
-})
- 
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
-
-
 
 
 
